7cde0e5c38ddbce105e524dc3c809fe37bf1bf89
John Wilger <johnwilger@gmail.com>
2008-01-28 23:12:39 +0000

    fixed bug in MkcolCommand related to previous change.

--- lib/pluggable_mongrel_webdav_handler/mkcol_command.rb
+++ lib/pluggable_mongrel_webdav_handler/mkcol_command.rb
@@ -1,7 +1,7 @@
 module PluggableMongrelWebdavHandler
   class MkcolCommand < Command
     def execute
-      return { :status => 415 } unless request_body.empty?
+      return { :status => 415 } unless request_body.read.empty?
       return { :status => 405 } unless resource.nil?
       return { :status => 409 } if parent_collection.nil?
       self.resource = parent_collection.make_collection( resource_name )
